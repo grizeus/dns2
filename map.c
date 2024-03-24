547289f331087b3a1fb071251d8087be0bc22a73
grizeus <zaytsewww@gmail.com>
2024-03-25 01:32:35 +0200

    add remove_from_map, change variable aliases names

--- map.c
+++ map.c
@@ -5,12 +5,16 @@
 
 #include "map.h"
 
-node_t* create_node(pair_value_t data) {
+map_node_t* create_map_node(const uint16_t id, const struct sockaddr_in* client_addr, const uint8_t* query) {
 
-    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+    map_node_t* new_node = (map_node_t*)malloc(sizeof(map_node_t));
 
     if (new_node != NULL) {
-        new_node->data = data;
+        new_node->data.transaction_id = id;
+        linked_list_t* list;
+        linked_list_init(list);
+        insert_into_list(list, client_addr, query);
+        new_node->data.client_info = list;
         new_node->color = RED;
         new_node->parent = NULL;
         new_node->left = NULL;
@@ -20,19 +24,23 @@ node_t* create_node(pair_value_t data) {
     return new_node;
 }
 
-void insert(map_t* map, pair_value_t data) {
+void save_client(map_t* map, const uint16_t id, const struct sockaddr_in* client_addr, const uint8_t* query) {
 
-    node_t* new_node = create_node(data);
+    map_node_t* new_node = create_map_node(id, client_addr, query);
 
-    node_t* parent = NULL;
-    node_t* current = map->root;
+    map_node_t* parent = NULL;
+    map_node_t* current = map->root;
 
     while (current != NULL) {
         parent = current;
-        if (new_node->data.transactionID < current->data.transactionID) {
+        if (new_node->data.transaction_id < current->data.transaction_id) {
             current = current->left;
-        } else {
+        } else if (new_node->data.transaction_id > current->data.transaction_id){
             current = current->right;
+        } else { // for node with the same transaction_id
+            insert_into_list(current->data.client_info, client_addr, query);
+            free(new_node);
+            return;
         }
     }
 
@@ -40,7 +48,7 @@ void insert(map_t* map, pair_value_t data) {
 
     if (parent == NULL) {
         map->root = new_node;
-    } else if (new_node->data.transactionID < parent->data.transactionID) {
+    } else if (new_node->data.transaction_id < parent->data.transaction_id) {
         parent->left = new_node;
     } else {
         parent->right = new_node;
@@ -49,13 +57,13 @@ void insert(map_t* map, pair_value_t data) {
     insert_fixup(map, new_node);
 }
 
-void insert_fixup(map_t* map, node_t* node) {
+void insert_fixup(map_t* map, map_node_t* node) {
 
     while (node->parent != NULL && node->parent->color == RED) {
 
         if (node->parent == node->parent->parent->left) {
 
-            node_t* uncle = node->parent->parent->right;
+            map_node_t* uncle = node->parent->parent->right;
             if (uncle != NULL && uncle->color == RED) {
                 node->parent->color = BLACK;
                 uncle->color = BLACK;
@@ -73,7 +81,7 @@ void insert_fixup(map_t* map, node_t* node) {
             }
         } else {
 
-            node_t* uncle = node->parent->parent->left;
+            map_node_t* uncle = node->parent->parent->left;
             if (uncle != NULL && uncle->color == RED) {
                 node->parent->color = BLACK;
                 uncle->color = BLACK;
@@ -95,10 +103,10 @@ void insert_fixup(map_t* map, node_t* node) {
     map->root->color = BLACK;
 }
 
-void right_rotate(map_t* map, node_t* node) {
+void right_rotate(map_t* map, map_node_t* node) {
 
-    node_t* pivot = node->left;
-    node_t* new_left_child = pivot->right;
+    map_node_t* pivot = node->left;
+    map_node_t* new_left_child = pivot->right;
 
     node->left = new_left_child;
     if (new_left_child != NULL) {
@@ -119,10 +127,10 @@ void right_rotate(map_t* map, node_t* node) {
     node->parent = pivot;
 }
 
-void left_rotate(map_t* map, node_t* node) {
+void left_rotate(map_t* map, map_node_t* node) {
 
-    node_t* pivot = node->right;
-    node_t* new_right_child = pivot->left;
+    map_node_t* pivot = node->right;
+    map_node_t* new_right_child = pivot->left;
 
     node->right = new_right_child;
     if (new_right_child != NULL) {
@@ -143,33 +151,109 @@ void left_rotate(map_t* map, node_t* node) {
     node->parent = pivot;
 }
 
-void inorder_traversal(node_t* node) {
+// void inorder_traversal(map_node_t* node) {
 
-    if (node != NULL) {
-        inorder_traversal(node->left);
-        printf("(%u, %s:%d)", node->data.transactionID,
-               inet_ntoa(node->data.client.sin_addr),
-               ntohs(node->data.client.sin_port));
-        inorder_traversal(node->right);
-    }
-}
+//     if (node != NULL) {
+//         inorder_traversal(node->left);
+//         printf("(%u, %s:%d)", node->data.transaction_id,
+//                inet_ntoa(node->data.client.sin_addr),
+//                ntohs(node->data.client.sin_port));
+//         inorder_traversal(node->right);
+//     }
+// }
 
-node_t* search(map_t* map, uint16_t key) {
+struct sockaddr_in* get_client(map_t* map, uint16_t id, const uint8_t* query, int query_len) {
 
-    node_t* current = map->root;
+    map_node_t* current = map->root;
 
-    while (current != NULL && current->data.transactionID != key) {
-        if (key < current->data.transactionID) {
+    while (current != NULL && current->data.transaction_id != id) {
+        if (id < current->data.transaction_id) {
             current = current->left;
         } else {
             current = current->right;
         }
     }
 
-    return current;
+    if (is_one_node(current->data.client_info)) {
+        return &current->data.client_info->head->client_addr;
+    } else {
+        list_node_t* req_node = search_by_query(current->data.client_info, query, query_len);
+        return req_node->client_addr;
+    }
 }
 
-static void destroy_node(node_t* root) {
+// Function to remove a node from the map
+void remove_client(map_t* map, uint16_t id, const uint8_t* query, int query_len) {
+    map_node_t* current = map->root;
+    map_node_t* parent = NULL;
+    map_node_t* node_to_remove = NULL;
+
+    // Find the node to remove
+    while (current != NULL) {
+        if (id == current->data.transaction_id) {
+            if (is_one_node(current->data.client_info)) {
+                node_to_remove = current;
+                break;
+            } else {
+                delete_from_list(current->data.client_info, query, query_len);
+                return;
+            }
+        } else if (id < current->data.transaction_id) {
+            parent = current;
+            current = current->left;
+        } else {
+            parent = current;
+            current = current->right;
+        }
+    }
+
+    if (node_to_remove == NULL) {
+        // Node with the given ID not found
+        return;
+    }
+
+    // Case 1: Node has no children
+    if (node_to_remove->left == NULL && node_to_remove->right == NULL) {
+        if (parent == NULL) {
+            // Root node
+            map->root = NULL;
+        } else if (parent->left == node_to_remove) {
+            parent->left = NULL;
+        } else {
+            parent->right = NULL;
+        }
+        free(node_to_remove);
+    }
+    // Case 2: Node has one child
+    else if (node_to_remove->left == NULL || node_to_remove->right == NULL) {
+        map_node_t* child = (node_to_remove->left != NULL) ? node_to_remove->left : node_to_remove->right;
+        if (parent == NULL) {
+            // Root node
+            map->root = child;
+        } else if (parent->left == node_to_remove) {
+            parent->left = child;
+        } else {
+            parent->right = child;
+        }
+        free(node_to_remove);
+    }
+    // Case 3: Node has two children
+    else {
+        map_node_t* successor = find_successor(node_to_remove->right);
+        node_to_remove->data = successor->data;
+        remove_client(map, successor->data.transaction_id, query, query_len);
+    }
+}
+
+// Function to find the successor of a node (used in case of removing a node with two children)
+static map_node_t* find_successor(map_node_t* node) {
+    while (node->left != NULL) {
+        node = node->left;
+    }
+    return node;
+}
+
+static void destroy_node(map_node_t* root) {
 
     if (root != NULL) {
         destroy_node(root->left);
--- map.h
+++ map.h
@@ -3,41 +3,46 @@
 
 #include <netinet/in.h>
 #include <stdint.h>
+#include "linked_list.h"
 
-typedef struct pair_value pair_value_t;
-typedef struct node node_t;
+typedef struct transaction_record transaction_record_t;
+typedef struct map_node map_node_t;
 typedef struct map map_t;
+
 
 typedef enum color {
     RED,
     BLACK
 } color_t;
 
-struct pair_value {
 
-    uint16_t transactionID;
-    struct sockaddr_in client;
+
+struct transaction_record {
+
+    uint16_t transaction_id;
+    linked_list_t* client_info;
 };
 
-struct node {
-    pair_value_t data;
+struct map_node {
+    transaction_record_t data;
     color_t color;
-    node_t* parent;
-    node_t* left;
-    node_t* right;
+    map_node_t* parent;
+    map_node_t* left;
+    map_node_t* right;
 };
 
 struct map {
-    node_t* root;
+    map_node_t* root;
 };
 
-node_t* create_node(pair_value_t data);
-void insert(map_t* map, pair_value_t data);
-void insert_fixup(map_t* map, node_t* node);
-void right_rotate(map_t* map, node_t* node);
-void left_rotate(map_t* map, node_t* node);
-void inorder_traversal(node_t* node);
+map_node_t* create_map_node(const uint16_t id, const struct sockaddr_in* client_addr, const uint8_t* query);
+void save_client(map_t* map, const uint16_t id, const struct sockaddr_in* client_addr, const uint8_t* query);
+void insert_fixup(map_t* map, map_node_t* node);
+void right_rotate(map_t* map, map_node_t* node);
+void left_rotate(map_t* map, map_node_t* node);
+void inorder_traversal(map_node_t* node);
 void destroy_map(map_t* map);
-node_t* search(map_t* map, uint16_t key);
+struct sockaddr_in* get_client(map_t* map, uint16_t id, const uint8_t* query, int query_len);
+void remove_client(map_t* map, uint16_t id, const uint8_t* query, int query_len);
 
 #endif // !MAP_H
