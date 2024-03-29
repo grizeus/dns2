9666665af4e3241a51b19bdbe717d0a10bcf8301
grizeus <zaytsewww@gmail.com>
2024-03-29 21:57:28 +0200

    rework linked_list

--- linked_list.c
+++ linked_list.c
@@ -1,14 +1,12 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "linked_list.h"
 
-list_node_t* create_list_node(void* data) {
+list_t* list_new(void* data) {
 
-    list_node_t* new_node = (list_node_t*)malloc(sizeof(list_node_t));
+    list_t* new_node = (list_t*)malloc(sizeof(list_t));
     if (!new_node) {
         perror("Memory allocation for list failed");
         free(new_node);
@@ -16,78 +14,27 @@ list_node_t* create_list_node(void* data) {
     }
 
     new_node->data = data;
-
-    new_node->prev = NULL;
     new_node->next = NULL;
 
     return new_node;
 }
 
-void linked_list_init(linked_list_t* list) {
+void list_add(list_t** head, void* data) {
 
-    list->head = NULL;
-    list->tail = NULL;
-}
+    list_t* new_node = list_new(data);
 
-void insert_into_list(linked_list_t* list, void* data) {
-
-    list_node_t* new_node = create_list_node(data);
-
-    if (list->head == NULL) {
-        list->head = new_node;
-        list->tail = new_node;
+    if (*head == NULL) {
+        *head = new_node;
     } else {
-        new_node->next = list->head;
-        list->head->prev = new_node;
-        list->head = new_node;
+        new_node->next = *head;
+        *head = new_node;
     }
 }
 
-int compare_by_query(transaction_info_t* first, transaction_info_t* second, ssize_t len) {
 
-    if (memcmp(first->query, second->query, len)) {
-        return 1;
-    }
-    return 0;
-}
+void list_iterate(list_t* list, void (*iter)(void*)) {
 
-void delete_from_list(linked_list_t* list, void* key, ssize_t len, int(*compare)(void*, void*, ssize_t)) {
-
-    if (list == NULL || compare == NULL) {
-        return;
-    }
-
-    list_node_t* current = list->head;
-
-    while (current != NULL) {
-        if ((*compare)(current->data, key, len)) {
-
-            // Adjust pointers of adjacent nodes
-            if (current->prev != NULL) {
-                current->prev->next = current->next;
-            } else {
-                // Node to delete is the head of the list
-                list->head = current->next;
-            }
-
-            if (current->next != NULL) {
-                current->next->prev = current->prev;
-            } else {
-                // Node to delete is the tail of the list
-                list->tail = current->prev;
-            }
-
-            free(current);
-            return;
-        }
-
-        current = current->next;
-    }
-}
-
-void list_iterate(linked_list_t* list, void (*iter)(void*)) {
-
-    list_node_t* current = list->head;
+    list_t* current = list;
 
     while (current != NULL) {
         (*iter)(current->data);
@@ -95,7 +42,61 @@ void list_iterate(linked_list_t* list, void (*iter)(void*)) {
     }
 }
 
-int is_one_node(const linked_list_t* list) {
+list_t* list_find(list_t* list, void* key, int(*compare)(void*, void*)) {
 
-    return (list->head != NULL && list->head == list->tail);
+    list_t* current = list;
+    while (current != NULL) {
+        if (compare(current->data, key) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+
+    return NULL;
 }
+
+void list_delete(list_t** list, void* key, int(*compare)(void*, void*)) {
+
+    if (*list == NULL || compare == NULL) {
+        return;
+    }
+
+    list_t* current = *list;
+    list_t* prev = NULL;
+
+    while (current != NULL) {
+        if ((*compare)(current->data, key)) {
+
+            // Adjust pointers of adjacent nodes
+            if (prev != NULL) {
+                prev->next = current->next;
+            } else {
+                *list = current->next;
+            }
+
+            free(current);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+void list_clear(list_t** head) {
+
+    list_t* current = *head;
+    list_t* next = NULL;
+
+    while (current != NULL) {
+        
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
+}
+// int is_one_node(const linked_list_t* list) {
+//
+//     return (list->head != NULL && list->head == list->tail);
+// }
--- linked_list.h
+++ linked_list.h
@@ -1,37 +1,16 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include <netinet/in.h>
-#include <stdint.h>
-
-typedef struct list_node list_node_t;
-typedef struct linked_list linked_list_t;
-typedef struct transaction_info transaction_info_t;
-
-struct transaction_info {
-
-    const struct sockaddr_in* client_addr;
-    const uint8_t* query;
-};
-
-struct list_node {
-
+typedef struct list {
     void* data;
-    list_node_t* prev;
-    list_node_t* next;
-};
+    struct list* next;
+} list_t;
 
-struct linked_list {
+list_t* list_new(void* data);
+void list_add(list_t** head, void* data);
+void list_iterate(list_t* head, void(*iter)(void*));
+list_t* list_find(list_t* head, void* key, int(*compare)(void*, void*));
+void list_delete(list_t** head, void* key, int(*compare)(void*, void*));
+void list_clear(list_t** head);
 
-    list_node_t* head;
-    list_node_t* tail;
-};
-
-list_node_t* create_list_node(void* data);
-void linked_list_init(linked_list_t* list);
-void insert_into_list(linked_list_t* list, void* data);
-int compare_by_query(transaction_info_t* first, transaction_info_t* second, ssize_t len);
-void delete_from_list(linked_list_t* list, void* key, ssize_t len, int(*compare)(void*, void*, ssize_t));
-void list_iterate(linked_list_t* list, void (*iter)(void*));
-int is_one_node(const linked_list_t* list);
-#endif // !LINKED_LIST_H
+#endif // !LIST_H
