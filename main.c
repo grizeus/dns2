bf1dd1e1c6bf641af4c228e05bb0ac4700decc8a
grizeus <zaytsewww@gmail.com>
2024-04-15 00:13:04 +0300

    fix main

--- main.c
+++ main.c
@@ -14,27 +14,43 @@
 #include "dns_parser.h"
 #include "map.h"
 
+
+
 int main(int argc, char** argv) {
 
+    if (argc != 3) {
+        printf("Program must run with %s local_address upstream\n", argv[0]);
+        return 1;
+    }
+    char* local = argv[1];
+    if (local != NULL) {
+        printf("local %s\n", local);
+    }
+    char* upstream = argv[2];
+    if (upstream != NULL) {
+        printf("upstream %s\n", upstream);
+    }
+    server_config_t config;
+    config.upstream_name = upstream;
+    config.local_address = local;
     map_t* clients = map_create();
     map_t* lookup = map_create();
-    server_config_t config;
-    char** black_list;
-    initialize("file.ini", black_list, &config);
+    char** black_list = NULL;
+    initialize_black_list("config.ini", &black_list);
 
     int sockfd, dns_sockfd;
-    struct sockaddr_in server_addr, dns_addr, *client_addr;
+    struct sockaddr_in server_addr, dns_addr, client_addr;
 
-    setup_sockets(&sockfd, &dns_sockfd, &server_addr, client_addr, &dns_addr, &config);
+    setup_sockets(&sockfd, &dns_sockfd, &server_addr, &client_addr, &dns_addr, &config);
 
-    printf("Server listening on port %d...\n", config.port);
+    printf("Server listening...\n");
 
     // Set the socket to non-blocking mode
     if (fcntl(sockfd, F_SETFL, O_NONBLOCK) == -1) {
         perror("Failed to set non-blocking mode");
         exit(1);
     }
-    
+
     while (1)
     {
         ssize_t recv_cl_len;
@@ -44,11 +60,11 @@ int main(int argc, char** argv) {
         uint32_t client_id;
         uint8_t* query;
 
-        if ((message = receive_from(sockfd, client_addr, &recv_cl_len)) != NULL) {
+        if ((message = receive_from(sockfd, &client_addr, &recv_cl_len)) != NULL) {
 
             char* dns_name = parse_query(message, recv_cl_len, &dns_id, &client_id);
             if (in_list(dns_name, black_list)) {
-                send_to(sockfd, "Error", 6, client_addr);
+                send_to(sockfd, "Error", 6, &client_addr);
                 free(dns_name);
                 continue;
             }
@@ -59,23 +75,29 @@ int main(int argc, char** argv) {
             binary_string_t* answer = map_find(lookup, dns_id);
             if (!answer) {
                 if (send_to(dns_sockfd, message, recv_cl_len, &dns_addr)) {
-                    map_add(clients, client_id, client_addr, NULL);
+                    map_add(clients, client_id, &client_addr, NULL);
                     free(message);
                 }
             } else {
                 size_t response_len;
                 char* response = build_response(message, recv_cl_len, answer, &response_len);
-                send_to(sockfd, response, response_len, client_addr);
+                send_to(sockfd, response, response_len, &client_addr);
                 free(response);
                 free(message);
             }
             is_received = 1;
         }
         binary_string_t response;
-        if (message == receive_from(dns_sockfd, client_addr, &recv_cl_len)) {
+        if (message == receive_from(dns_sockfd, &client_addr, &recv_cl_len)) {
             parse_responce(message, recv_cl_len, &response, &dns_id, &client_id);
-            client_addr = map_find(clients, client_id);
-            if (send_to(sockfd, message, recv_cl_len, client_addr)) {
+            struct sockaddr_in* result = map_find(clients, client_id);
+            if (result){
+                client_addr = *result;
+            } else {
+                perror("Address not found");
+                continue;
+            }
+            if (send_to(sockfd, message, recv_cl_len, &client_addr)) {
                 map_add(lookup, dns_id, &response, NULL);
                 map_delete(clients, client_id, NULL, NULL, NULL);
             }
