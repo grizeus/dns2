97c25823cb9a489d17a6b9fe9a78d9f86ffb84c5
grizeus <zaytsewww@gmail.com>
2024-04-17 20:08:25 +0300

    minor changes

--- utility.c
+++ utility.c
@@ -5,7 +5,7 @@
 
 #include "utility.h"
 
-#define PORT     8080
+#define PORT     8888
 #define DNS_PORT 53
 
 static void query_setup(char query[]);
@@ -15,33 +15,34 @@ void setup_sockets(int* sockfd, int* dns_sockfd,
                     struct sockaddr_in* client_addr,
                     struct sockaddr_in* dns_addr,
                     server_config_t* config) {
-    
+
     if ((*sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(1);
     }
-    
+
     if ((*dns_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         perror("DNS socket creation failed");
         exit(1);
     }
-    
+
     memset(server_addr, 0, sizeof(*server_addr));
     memset(client_addr, 0, sizeof(*client_addr));
     memset(dns_addr, 0, sizeof(*dns_addr));
 
     server_addr->sin_family = AF_INET;
     server_addr->sin_port = htons(PORT);
-    inet_pton(AF_INET, config->local_address, &(server_addr->sin_addr));
+    inet_pton(AF_INET, config->local_address, &server_addr->sin_addr);
 
     dns_addr->sin_family = AF_INET;
     dns_addr->sin_port = htons(DNS_PORT);
-    inet_pton(AF_INET, config->upstream_name, &(dns_addr->sin_addr));
+    inet_pton(AF_INET, config->upstream_name, &dns_addr->sin_addr);
 
-    if (bind(*sockfd, (const struct sockaddr *)server_addr, sizeof(struct sockaddr_in)) < 0) {
+    if (bind(*sockfd, (const struct sockaddr *)server_addr, sizeof(*server_addr)) < 0) {
         perror("Binding failed");
         exit(1);
     }
+    puts("Sockets setup is done");
 }
 
 char* build_response(char* initial_query, size_t query_size, binary_string_t* answer, size_t* new_size) {
@@ -51,7 +52,7 @@ char* build_response(char* initial_query, size_t query_size, binary_string_t* an
     }
     // allocate memory for response
     *new_size = query_size + answer->size;
-    char* new_response = malloc(*new_size);
+    char* new_response = (char*)malloc(*new_size);
     if (new_response == NULL) {
         return NULL;
     }
