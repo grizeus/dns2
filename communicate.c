0be2808b108b729e33998d43ddbe0b5d224ebc54
grizeus <zaytsewww@gmail.com>
2024-04-24 23:01:55 +0300

    change dynamic memory to static in receive_from()

--- communicate.c
+++ communicate.c
@@ -13,8 +13,8 @@ ssize_t send_to(int fd, const char* msg, ssize_t msg_len, struct sockaddr_in* ad
 
 char* receive_from(int fd, struct sockaddr_in* client_addr, ssize_t* recv_len) {
 
-    char buffer[MAX_BUFF_SIZE];
-
+    static char buffer[MAX_BUFF_SIZE];
+    memset(&buffer, 0, MAX_BUFF_SIZE);
     socklen_t client_addr_len = sizeof(*client_addr);
     *recv_len = recvfrom(fd, buffer, MAX_BUFF_SIZE, 0, (struct sockaddr*)client_addr, &client_addr_len);
 
@@ -24,11 +24,11 @@ char* receive_from(int fd, struct sockaddr_in* client_addr, ssize_t* recv_len) {
         }
         return NULL;
     }
+    FILE* log = fopen("log.txt", "a");
+    fprintf(log, "Received from %s:%d with len %zd\n", inet_ntoa(client_addr->sin_addr), htons(client_addr->sin_port), *recv_len);
+    printf("Received from %s:%d with len %zd\n", inet_ntoa(client_addr->sin_addr), htons(client_addr->sin_port), *recv_len);
+    printf("%s\n", buffer);
+    fclose(log);
 
-    printf("Received from %s:%d\n", inet_ntoa(client_addr->sin_addr), htons(client_addr->sin_port));
-
-    char* msg = malloc(*recv_len);
-    memcpy(msg, buffer, *recv_len);
-
-    return msg;
+    return buffer;
 }
--- communicate.h
+++ communicate.h
@@ -4,7 +4,7 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 
-#define MAX_BUFF_SIZE  2048
+#define MAX_BUFF_SIZE 1024
 
 char* receive_from(int fd, struct sockaddr_in* client_addr, ssize_t* recv_len);
 ssize_t send_to(int fd, const char* msg, ssize_t msg_len, struct sockaddr_in* addr);
